945e0860b5cf333dc2166daee61dcd6c82a59747
wangwenjie <wangwenjie@letv.com>
2016-02-02 13:43:47 +0800

    update router

---
+++ src/component/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+
+const About = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Users</h1>
+      </div>
+    );
+  },
+});
+
+export default About;
--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -1,6 +1,17 @@
 import '../common/lib';
 import App from '../component/App';
-import ReactDOM from 'react-dom';
+import About from '../component/About';
+import { render } from 'react-dom';
 import React from 'react';
+import { Router, Route, browserHistory } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('react-content'));
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="about" component={About}/>
+      <Route path="*" component={App}/>
+    </Route>
+  </Router>
+), document.getElementById('react-content'));
+
+//ReactDOM.render(<App />, document.getElementById('react-content'));
