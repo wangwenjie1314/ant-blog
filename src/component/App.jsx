a16a07c0e02f16f7aa1bcf6aa613d4e316fcf760
wangwenjie <wangwenjie@letv.com>
2016-02-15 16:43:54 +0800

    update

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import Header from './common/header';
 import Footer from './common/footer';
 import Home from './Home';
+import { QueueAnim } from 'antd';
 
 const App = React.createClass({
   render() {
     return (
-    	<div>
-	        <Header />
-	  	    <div className="clearfix" style={{ minHeight:500,paddingTop:20 }}>
+    	<QueueAnim delay={500}>
+	        <Header key="a"/>
+	  	    <div key="b" className="clearfix" style={{ minHeight:500,paddingTop:20 }}>
 	  	    	{this.props.children || <Home/>}
 	  	    </div>
-	      	<Footer />
-	    </div>
+	      	<Footer key="c"/>
+	    </QueueAnim>
     );
   }
 });
--- src/component/blog/list.jsx
+++ src/component/blog/list.jsx
@@ -1,16 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { Button } from 'antd';
+import { Row, Col,Button,Table } from 'antd';
 
 import './blog';//引入less
+
+const columns = [{
+  title: '姓名',
+  dataIndex: 'name',
+  render: function (text) {
+    return <a href="#">{text}</a>;
+  }
+}, {
+  title: '年龄',
+  dataIndex: 'age'
+}, {
+  title: '住址',
+  dataIndex: 'address'
+}];
+
+const data = [];
+for (let i = 0; i < 46; i++) {
+  data.push({
+    key: i,
+    name: '李大嘴' + i,
+    age: 32,
+    address: '西湖区湖底公园' + i + '号'
+  });
+}
+
+const pagination = {
+  total: data.length,
+  current: 1,
+  showSizeChanger: true,
+  onShowSizeChange: function (current, pageSize) {
+    console.log('Current: ', current, '; PageSize: ', pageSize);
+  },
+  onChange: function (current) {
+    console.log('Current: ', current);
+  }
+};
 
 const BlogList = React.createClass({
   render() {
     return (
     	<div>
-    		<Link to="/blog/add"><Button type="primary">新增博客form1</Button></Link>
-    		<Link to="/blog/add2"><Button type="primary">新增博客form2</Button></Link>
-    		<div>这里是list</div>
+    		<Row type="flex" justify="center">
+				<Col span="4">
+					<Link to="/blog/add"><Button type="primary">新增博客form1</Button></Link>
+				</Col>
+				<Col span="4">
+					<Link to="/blog/add2"><Button type="primary">新增博客form2</Button></Link>
+				</Col>
+		    </Row>
+    		<Table columns={columns} dataSource={data} pagination={pagination} />
     	</div>
     );
   },
